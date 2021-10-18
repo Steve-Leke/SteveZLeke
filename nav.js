7d8d17bdfe849e7599b0f8df59fa1546bdc643ae
Steve-Leke <43915535+Steve-Leke@users.noreply.github.com>
2021-10-18 09:38:47 -0700

    New NavBar

--- nav.js
+++ nav.js
@@ -1,9 +1,25 @@
-$(function () {
-    var includes = $('[data-include]')
-    $.each(includes, function () {
-      var file = '/' + $(this).data('include') + '.html'
-      $(this).load(file)
-    })
+// $(function () {
+//     var includes = $('[data-include]')
+//     $.each(includes, function () {
+//       var file = '/' + $(this).data('include') + '.html'
+//       $(this).load(file)
+//     })
 
-    document.title = "Tech Connect"
-  })
+//     document.title = "Tech Connect"
+//   })
+
+fetch("nav.html")
+    .then(stream => stream.text())
+    .then(text => defineNavBar(text));
+
+function defineNavBar(html) {
+    class NavBar extends HTMLElement {
+        constructor() {
+            super();
+
+            var shadow = this.attachShadow({mode: 'open'});
+            shadow.innerHTML = html;
+        }
+    }
+    customElements.define('nav-bar', NavBar);
+} 
